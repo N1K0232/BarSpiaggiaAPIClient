128a7e472a1b83eaa81b70d16dad299b73ce50e1
Nicola42 <60029977+Nicola42@users.noreply.github.com>
2021-09-09 17:08:49 +0200

    added classes to manage api methods

---
+++ samples/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using BarSpiaggiaAPIClient.Meteo.Core;
+using BarSpiaggiaAPIClient.Meteo.Models;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            MeteoClient meteoClient = new MeteoClient();
+            await meteoClient.SearchAsync("Bologna");
+            Information info = meteoClient.Info;
+            Location location = meteoClient.Location;
+        }
+    }
+}
---
+++ src/BarSpiaggiaApiClient/APIMethods/Method.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarSpiaggiaApiClient.APIMethods
+{
+    public abstract class Method
+    {
+        internal HttpClient client;
+
+        protected Method()
+        {
+            client = null;
+            CreateConnection();
+        }
+
+        protected abstract void CreateConnection();
+    }
+}
